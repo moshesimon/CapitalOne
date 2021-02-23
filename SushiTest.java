5dbb581c0de5616ef8f3e9acf35f5169c5678676
moshesimon <52634041+moshesimon@users.noreply.github.com>
2021-02-23 22:46:43 +0000

    Update SushiTest.java

--- SushiTest.java
+++ SushiTest.java
@@ -4,9 +4,15 @@ public class SushiTest {// Test Class
 
     public void printMenu(){
         System.out.println("Which Sushi would you like?");
-        System.out.println("1) Spicy Tuna");
+        System.out.println("1) Tuna");
         System.out.println("2) California Roll");
         System.out.println("3) Philadelphia Roll");
+    }
+
+    public void printTunaOptions(){
+        System.out.println("Which type of tuna roll would you like?");
+        System.out.println("1) Raw Tuna");
+        System.out.println("2) Spicy Tuna");
     }
 
     public int options(Scanner input){
@@ -16,6 +22,28 @@ public class SushiTest {// Test Class
             try{
                 option = input.nextInt();
                 if(Arrays.asList(1,2,3).contains(option)){
+                    break;
+                }
+                else{
+                    System.out.println("Please make sure the option you entered is listed:");
+                }
+            }
+            catch(InputMismatchException inputMismatchException){
+                System.err.printf("\nExeptions: %s\n", inputMismatchException);
+                System.out.println("Please only enter an integer");
+                input.nextLine();
+            }
+        }
+        return option;
+    }
+
+    public int tunaOptions(Scanner input){
+        int option;
+        while(true){
+            printTunaOptions();
+            try{
+                option = input.nextInt();
+                if(Arrays.asList(1,2).contains(option)){
                     break;
                 }
                 else{
@@ -45,8 +73,16 @@ public class SushiTest {// Test Class
 
         int option = sushiTest.options(input);
         if(option == 1){
-            BuildSushi buildSpicySushi = new BuildSpicyTuna();// Polymorphism
-            order.setSushiBuilder(buildSpicySushi);
+            int tunaOption = sushiTest.tunaOptions(input);
+            if(tunaOption == 1){
+                BuildSushi buildRawTuna = new BuildTuna();// Polymorphism
+                order.setSushiBuilder(buildRawTuna);
+            }
+            else if(tunaOption == 2){
+                BuildSushi buildSpicyTuna = new BuildSpicyTuna();// Polymorphism
+                order.setSushiBuilder(buildSpicyTuna);
+            }
+
         }
         else if(option == 2){
             BuildSushi buildCalSushi = new BuildCaliforniaRoll();// Polymorphism
