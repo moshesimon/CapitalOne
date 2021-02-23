192bcb11424e2deff03c94b18995baa20aef31cc
moshesimon <52634041+moshesimon@users.noreply.github.com>
2021-02-23 11:38:24 +0000

    Update BuildSushi.java

--- BuildSushi.java
+++ BuildSushi.java
@@ -1,10 +1,14 @@
 public interface BuildSushi {
 
-
     public void createNewSushi();
+
     public Sushi getSushi();
+
     public void buildName();
+
     public void buildFish();
+
     public void buildVeg();
+    
     public void buildSauce();
 }
